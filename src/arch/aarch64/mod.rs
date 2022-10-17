814f4ca0620d75501455bb31fe53ba08f96c642d
Reto Achermann <achreto@gmail.com>
2022-10-17 15:25:48 -0700

    armv8: implementing getting the timer registers
    Signed-off-by: Reto Achermann <achreto@gmail.com>

--- src/arch/aarch64/mod.rs
+++ src/arch/aarch64/mod.rs
@@ -1,4 +1,2 @@
-extern crate armv8;
-
 pub mod rtc;
 pub mod tsc;
--- src/arch/aarch64/tsc.rs
+++ src/arch/aarch64/tsc.rs
@@ -1,12 +1,16 @@
+use cortex_a::{asm::barrier, registers::*};
+use tock_registers::interfaces::Readable;
+
 lazy_static! {
     /// TSC Frequency in Hz
     pub static ref TSC_FREQUENCY: u64 = {
-        panic!("not implemented");
+        let freq = CNTFRQ_EL0.get()
     };
 
 }
 
 #[inline]
 pub fn precise_time_ns() -> u64 {
-    panic!("not implemented");
+    barrier::isb(barrier::SY);
+    let cnt = CNTPCT_EL0.get();
 }
