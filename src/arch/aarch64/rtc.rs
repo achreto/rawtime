aba36052c331000c8837704bec07956f32d791fa
Reto Achermann <achreto@gmail.com>
2022-10-17 16:02:43 -0700

    aarch64: return 1900-01-01 00:00:00 as start time
    Signed-off-by: Reto Achermann <achreto@gmail.com>

--- src/arch/aarch64/rtc.rs
+++ src/arch/aarch64/rtc.rs
@@ -2,10 +2,10 @@ use crate::DateTime;
 
 pub fn now() -> DateTime {
     DateTime {
-        sec: 1 as u8,
-        min: 1 as u8,
+        sec: 0 as u8,
+        min: 0 as u8,
         day: 1 as u8,
-        hour: 1 as u8,
+        hour: 0 as u8,
         mon: 1 as u8,
         year: 1900 as u64,
     }
